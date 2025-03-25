e52c23a7394378a5fa2599d1cab4f95044362a7f
Syndesi <soerenklein98@gmail.com>
2025-03-25 23:07:59 +0100

    wip

---
+++ src/Type/Definition/Action.ts
@@ -0,0 +1,15 @@
+type ActionManifest<C = object> = {
+  id: string;
+  name: string;
+  description: string;
+  tags: string[];
+  configuration?: C;
+};
+
+interface Action<T = unknown, C = object> {
+  getManifest(): ActionManifest<C>;
+  supports(data: unknown): boolean;
+  execute(data: unknown, configuration?: C): Promise<T>;
+}
+
+export { ActionManifest, Action };

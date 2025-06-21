9dcb5c59d273bc59faf936548581354cbd2a77fa
Syndesi <soerenklein98@protonmail.com>
2025-06-21 17:27:17 +0200

    remove deprecated browser events

--- src/BrowserEvent/User/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DeleteTokenEvent.js';
-export * from './GetMeEvent.js';
-export * from './GetTokenEvent.js';
-export * from './PostChangePasswordEvent.js';
-export * from './PostRegisterEvent.js';
-export * from './PostTokenEvent.js';
